26188bdc92ac0a1fbf3dab7543340e62dde1eb1a
Kalebo Bayaga <kalebobayag@gmail.com>
2025-05-02 07:01:07 -0700

    Resolved merge conflicts in next.config.js and package.json

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
-<<<<<<< HEAD
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+};
 
-module.exports = nextConfig
-=======
-module.exports = { reactStrictMode: true };
->>>>>>> 3b86b0f0e5f59fac777cd79b3928ea4656eea8fa
+module.exports = nextConfig;
