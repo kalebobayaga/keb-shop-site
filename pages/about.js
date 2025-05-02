d86f993e5e8d001cd5c40553c05d781043bfcb3e
Kalebo Bayaga <kalebobayag@gmail.com>
2025-05-02 06:46:15 -0700

    Initial working version of KEB Shop

---
+++ pages/about.js
@@ -0,0 +1,8 @@
+export default function About() {
+  return (
+    <div>
+      <h1>About KEB Engineers</h1>
+      <p>We specialize in industrial automation, instrumentation, and electronics.</p>
+    </div>
+  );
+}
